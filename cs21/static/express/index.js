01192291e2f65f99cd858fe4d49679bf6a2bf782
Vadim G.B <vadimgb@yandex.ru>
2022-12-12 21:23:58 +0700

    express

--- cs21/static/express/index.js
+++ cs21/static/express/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 const app = express()
 const port = process.env.PORT | 8080 
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
